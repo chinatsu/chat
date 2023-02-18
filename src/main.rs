e7835140b60281abb137f6a11220940144db3a47
Kent D <kent.daleng@gmail.com>
2023-02-18 20:22:37 +0100

    format according to current local terminal width

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
-use ansi_term::{ANSIString, Color::Fixed, Color::RGB};
+use ansi_term::{
+    ANSIString, Color::Fixed, Color::RGB,
+};
 use std::num::ParseIntError;
 use textwrap::{fill, termwidth, Options};
 use twitchchat::{
@@ -9,6 +11,7 @@ use twitchchat::{
     Status, UserConfig,
 };
 
+type Hex = Result<Vec<u8>, ParseIntError>;
 
 fn get_color(hex: &str) -> ansi_term::Color {
     let hex = if hex.starts_with('#') {
@@ -16,15 +19,17 @@ fn get_color(hex: &str) -> ansi_term::Color {
     } else {
         hex
     };
-    
+
     if hex.len() != 6 {
         return RGB(100, 100, 100);
     }
 
     match (0..hex.len())
         .step_by(2)
-        .map(|i| u8::from_str_radix(&hex[i..i + 2], 16))
-        .collect::<Result<Vec<u8>, ParseIntError>>()
+        .map(|i| {
+            u8::from_str_radix(&hex[i..i + 2], 16)
+        })
+        .collect::<Hex>()
     {
         Ok(v) => RGB(v[0], v[1], v[2]),
         Err(_) => Fixed(15),
@@ -48,9 +53,11 @@ async fn connect(
     channel: &String,
 ) -> anyhow::Result<AsyncRunner> {
     let connector = Connector::twitch()?;
-    let mut runner =
-        AsyncRunner::connect(connector, user_config)
-            .await?;
+    let mut runner = AsyncRunner::connect(
+        connector,
+        user_config,
+    )
+    .await?;
     let _ = runner.join(channel).await?;
 
     Ok(runner)
@@ -100,7 +107,10 @@ async fn show_message(msg: Privmsg<'_>) {
 
     let message = format!(
         "{} {}",
-        colored_nick(nick, msg.tags().get("color")),
+        colored_nick(
+            nick,
+            msg.tags().get("color")
+        ),
         msg.data()
     );
 
@@ -109,14 +119,14 @@ async fn show_message(msg: Privmsg<'_>) {
 }
 
 fn main() -> anyhow::Result<()> {
-    let args: Vec<String> = std::env::args()
-        .collect();
+    let args: Vec<String> =
+        std::env::args().collect();
 
     if args.len() < 2 {
         println!(
-            "Please specify desired Twitch channel"
+            "Specify desired Twitch channel"
         );
-        return Ok(())
+        return Ok(());
     }
     let channel = &args[1];
     let fut = async move {
@@ -124,9 +134,11 @@ fn main() -> anyhow::Result<()> {
             .anonymous()
             .capabilities(&[Capability::Tags])
             .build()?;
-        let runner =
-            connect(&config, &channel.to_string())
-                .await?;
+        let runner = connect(
+            &config,
+            &channel.to_string(),
+        )
+        .await?;
         read_loop(runner).await
     };
 
